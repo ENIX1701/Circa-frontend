bc2bae2241369129f27659e56b82d4ac05eca699
ENIX1701 <enix1701@gmail.com>
2026-04-21 10:45:51 +0200

    feat: solid color background

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import MobileNavbar from '@/components/MobileNavbar.vue'
 
 <template>
   <div
-    class="min-h-screen flex flex-col lg:flex-row text-white bg-[url(https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpaperaccess.com%2Ffull%2F628286.jpg&f=1&nofb=1&ipt=d70b28dd76d02cc78b5cba97efb2aac51f420e02718c216cb1121618c151bf43)]"
+    class="min-h-screen flex flex-col lg:flex-row text-white bg-gray-800"
   >
     <MobileNavbar class="block lg:hidden shrink-0" />
     <DesktopSidebar class="hidden lg:w-80 lg:block shrink-0 my-8 mx-4 [calc(100vh-4rem)]" />
